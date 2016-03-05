1adcd269663cbf17866f0e5de5ae23d82a679b3d
Christian Schwarz <christian.schwarz@refactory.at>
2016-03-05 20:47:29 +0100

    Prepare tag support

--- update-version.ps1
+++ update-version.ps1
@@ -1,5 +1,13 @@
-$versionFile = "$env:APPVEYOR_BUILD_FOLDER\version.txt"
-$version = [IO.File]::ReadAllText($versionFile)
-$version = "$version-ci$env:APPVEYOR_BUILD_NUMBER"
+$version = ""
+
+if($env:APPVEYOR_REPO_TAG -eq 'True') {
+	$version = $env:APPVEYOR_REPO_TAG_NAME   
+}
+else {
+	$versionFile = "$env:APPVEYOR_BUILD_FOLDER\version.txt"
+	$version = [IO.File]::ReadAllText($versionFile)
+	$version = "$version-ci$env:APPVEYOR_BUILD_NUMBER"	
+}
+
 Add-AppveyorMessage -Message "Version: $version"
 Update-AppveyorBuild -Version $version
